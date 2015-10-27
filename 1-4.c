e8168c2be98a1222f2bac6adea3589cb1eeda6e1
killerhack <zhouzhitong2012@163.com>
2015-10-27 15:50:37 +0800

    1-4

---
+++ 1-4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define MAX 300
+#define STEP 20
+main()
+{
+float huashi,ceshi=0;
+	printf("=====TEMPRETURE CONVERT TABLE=====");
+	while(huashi<MAX){
+	huashi=9.0/5*ceshi+32;
+	printf("%6.2f\t%6.2f\t\n",ceshi,huashi);
+	ceshi+=STEP;
+	}
+}
