889dfc5e069aef489218ceec27ce53a9ad9b4db8
killerhack <zhouzhitong2012@163.com>
2015-10-30 21:40:30 +0800

    save

--- 1-17.c
+++ 1-17.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
-main(){
-    int c,i=0;
-    char x[1000]={0};
-    while((c=getchar())!=EOF){
-        while((c=getchar())!='\n')
-        {
-            i++;
-            x[i]=c;
-        }
-       
-        if (i>80){
-           for(int j=0;j<i;j++) {
-               putchar(x[j]);
-           }
-            
-        }
-        i=0;
-        
+#define MAXLINE 1000
+#define LONGEST 80
+int getline2(char a[],int m);
+int main(){
+    char line[MAXLINE];
+    char logest[MAXLINE];
+    int len;
+    while((len=getline(line,MAXLINE))>0)
+    if(len>LONGEST)
+        printf("%s",line);
+        return 0;
+}
+
+int getline2(char s[],int lim)
+{
+    int c, i;
+    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
     }
+    s[i] = '\0';
+    return i;
 }
