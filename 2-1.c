855060aa4a19adb1b160919816af983c35f47f87
killerhack <zhouzhitong2012@163.com>
2015-10-31 15:26:12 +0800

    2-1

---
+++ 2-1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+printf("minimum signed char is %d\nmaximum signed char is %d\nminimum signed short is %d\nmaximum signed short is %d\nminimum signed int is %d\nmaximum signed int is %d\nminimum signed long is %ld\nmaximum signed long is %ld\nmaximumimum unsigned char is %u\nmaximumimum unsigned short is %u\nmaximumimum unsigned int is %u\nmaximumimum unsigned long is %lu\n",SCHAR_MIN,SCHAR_MAX,SHRT_MIN,SHRT_MAX,INT_MIN,INT_MAX,LONG_MIN,LONG_MAX,UCHAR_MAX,USHRT_MAX,UINT_MAX,ULONG_MAX);	
+
+
+}
