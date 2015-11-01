d0ec92d704b3cdd97dd9b51e8e50d9394567c352
killerhack <zhouzhitong2012@163.com>
2015-11-01 18:45:54 +0800

    temp save

--- First_Week_Work.c
+++ First_Week_Work.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     printf("Hi,this is KILLERHACK's first week homework\n");
-    printf("Please enter an command,enter 0 to see usage\n");
+    
     int c;
-    int courses[6]={1,5,10,7,21,39,22};
+    int courses[7]={1,5,10,7,21,39,22};
     int i=j=0;
     while((c=getchar())!=EOF){
         switch(c){
@@ -19,14 +19,31 @@ int main(){
                     printf("our courses contains:\n");
                     for(;i<=6;i++)
                     printf("%3d\t",courses[i]);
+                    printf("\n");
                     i=0;
                     break;
             case '2':
                     for(;i<=6;i++)
                     j+=courses[i];
                     i=0;
-                    printf("the totall count of courses is %d",j);
+                    printf("the totall count of courses is %d\n",j);
                     j=0;
+            case '3':
+                    for(;i<=6;i++)
+                    j=(j>=courses[i])?j:s[i];
+                    printf("the MAX ID of course is %d\n",j);
+                    j=0;
+                    break;
+            case '4':
+                    printf("enter an ID,to search whether it is one of our course\n");
+                    scanf("%d",&j);
+                    for(;i<=6;i++)
+                        if(j==course[i])
+                        printf("yes,it is one of our course\n");
+            case '5':
+            default :
+            printf("Please enter an command,enter 0 to see usage\n");
+            break;
         }
     }
 }
