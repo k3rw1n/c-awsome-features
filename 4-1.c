6ed0e9e95b8f1ac4e15f52f8675278638b4b4fc2
killerhack <zhouzhitong2012@163.com>
2015-11-04 20:26:43 +0800

    fix problem of 4-1

--- 4-1.c
+++ 4-1.c
@@ -1,15 +1,13 @@
-int strrindex( char s[], char t )
+
+int strrindex(char s[], char t[])
 {
-  int i;
-  int count = -1;
- 
-  for(i=0; s[i] != '\0'; i++)
-  {
-    if(s[i] == t)
-    {
-      count = i;
-    }
-  }
- 
-  return count;
+	int i, j, k=0;
+	int pos=i;
+	for (i = 0; s[i] != '\0'; i++) {
+		for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+			;
+		if (k > 0 && t[k] == '\0')
+			pos=i;
+	}
+	return pos;
 }
