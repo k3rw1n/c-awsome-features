a90dd35d2c643c51acdd88db8cef51863abb0749
killerhack <zhouzhitong2012@163.com>
2015-11-16 10:21:00 +0800

    finished

--- lab3/judge.h
+++ lab3/judge.h
@@ -11,8 +11,6 @@ struct course{
     1,"Linux",5,"C++",10,"HTML",7,"HTML5",21,"NodeJS",39,"Shell",22,"Python"
 };
 
-
-
 void judge(int c){
     int i,j=0;
     switch(c){
@@ -23,7 +21,6 @@ void judge(int c){
                             "3.print the course whose name is longest\n"
                             "4.enter an course name,to search whether it is one of our course\n"
                             "5.quit the course\n");
-                   
                             break;
             case '1':
                     printf("our courses contains:\n");
@@ -37,41 +34,39 @@ void judge(int c){
                     fprintf(fp,"%3d%s\t",courses[i].id,courses[i].name);
                     break;
             case '2':
-                    
                     printf("the totall count of courses is %d\n",COURSES_NUMBER);
-                    
                     break;
             case '3':
                     {
                     char m[15];
                     for(i=0;i<COURSES_NUMBER;i++)
-                    // j=(j>=strlen(courses[i]))?j:strlen(courses[i]);
+                    
                     if(strlen(courses[i].name)>j)
                     {
                         j=strlen(courses[i].name);
                         strcpy(m,(courses+i)->name);
                     }
-                    
                     printf("the course who has the longest name is %s\n",m);
-                    // j=0;
                     break;
                     }
             case '4':
-                    printf("enter an course name,to search whether it is one of our course\n");
+                    printf("enter an course name and its ID,to search whether it is one of our course\n");
                     char x[15];
+                    printf("please enter the course name");
                     scanf("%s",x);
+                    printf("please enter the id");
+                    while(getchar()!='\n')
+                    ;
+                    
                     for(;i<COURSES_NUMBER;i++)
                         if(!strcmp(x, courses+i)){
                             printf("yes,it is one of our course\n");
-                            // j=0;
+                            
                             printf("It's %s",courses[i]);
                             break;
                         }
                         printf("No,we never heard this course\n");
-                        // j=0;
                         break;
-                    
-            //there needn't case '5':
                     
             default :
             printf("\n\n\nPlease enter an command,enter 0 to see usage\n>>");
