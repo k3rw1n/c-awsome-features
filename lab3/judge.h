e67a3af172d3ffbca64006ccacf8c0d7e3308267
killerhack <zhouzhitong2012@163.com>
2015-11-16 18:12:27 +0800

    modified

--- lab3/judge.h
+++ lab3/judge.h
@@ -15,6 +15,7 @@ void judge(int c){
     int i,j=0;
     switch(c){
             case '0':
+                    {
                     printf("0.help information\n"
                             "1.print all the class name of shiyanlou\n"
                             "2.print the totall count of courses\n"
@@ -22,29 +23,30 @@ void judge(int c){
                             "4.enter an course name,to search whether it is one of our course\n"
                             "5.quit the course\n");
                             break;
+                    }
             case '1':
+                    {
                     printf("our courses contains:\n");
-                    for(;i<COURSES_NUMBER;i++)
-                    printf("%3d%s\t",courses[i].id,courses[i].name);
+                    for(i=0;i<COURSES_NUMBER;i++)
+                    printf("%3d%s\t",courses[i]->id,courses[i]->name);
                     printf("\n");
                     // i=0;
-                    FILE *fp;
-                    fp=fopen("/tmp/courselist.txt","w");
-                    for(;i<COURSES_NUMBER;i++)
-                    fprintf(fp,"%3d%s\t",courses[i].id,courses[i].name);
                     break;
+                    }
             case '2':
+                    {
                     printf("the totall count of courses is %d\n",COURSES_NUMBER);
                     break;
+                    }
             case '3':
                     {
                     char m[15];
                     for(i=0;i<COURSES_NUMBER;i++)
                     
-                    if(strlen(courses[i].name)>j)
+                    if(strlen(courses[i]->name)>j)
                     {
-                        j=strlen(courses[i].name);
-                        strcpy(m,(courses+i)->name);
+                        j=strlen(courses[i]->name);
+                        strcpy(m,courses[i]->name);
                     }
                     printf("the course who has the longest name is %s\n",m);
                     break;
@@ -59,17 +61,23 @@ void judge(int c){
                     ;
                     
                     for(;i<COURSES_NUMBER;i++)
-                        if(!strcmp(x, courses+i)){
+                        if(!strcmp(x, (courses+i)->name)){
                             printf("yes,it is one of our course\n");
                             
-                            printf("It's %s",courses[i]);
+                            printf("It's %s",courses[i]->name);
                             break;
                         }
+                        if((i==COURSES_NUMBER-1)&&x[14]==0)
                         printf("No,we never heard this course\n");
                         break;
+                    }
                     
             default :
+            {
             printf("\n\n\nPlease enter an command,enter 0 to see usage\n>>");
             break;
+                
+            }
+}
 }
 void judge(int c);
