9c9d310c7e54ab4105ef15f8d2267bcb38f9927b
killerhack <zhouzhitong2012@163.com>
2015-11-22 13:34:57 +0800

    tmp

---
+++ lab4/judge.h
@@ -0,0 +1,15 @@
+#ifndef COURSES_NUMBER
+#define COURSES_NUMBER (sizeof courses /sizeof courses[0])
+#endif
+
+#include<string.h>
+
+struct course{
+    int id;
+    char * name;
+}courses[]={
+    1,"Linux",5,"C++",10,"HTML",7,"HTML5",21,"NodeJS",39,"Shell",22,"Python"
+};
+
+
+void judge(int c);
